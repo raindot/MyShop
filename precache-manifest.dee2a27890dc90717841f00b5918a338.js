3b2d0b9c561a72b9e2f7cf533b1e67d3f3a54305
Raindot <sakuhime24@gmail.com>
2020-08-28 19:38:09 +0800

    update 2020-08-28 19:38:09

---
+++ precache-manifest.dee2a27890dc90717841f00b5918a338.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4a31017b00b318f671",
+    "url": "css/app.7fccf016.css"
+  },
+  {
+    "revision": "6b3fb8089d551f240ba4",
+    "url": "css/chunk-7d6732d4.c849674c.css"
+  },
+  {
+    "revision": "3b73c5e355af8fb39e95",
+    "url": "css/chunk-7f015059.c2c68d34.css"
+  },
+  {
+    "revision": "d2202a193fd8476d4f02",
+    "url": "css/chunk-vendors.096ddd0e.css"
+  },
+  {
+    "revision": "1f66711ab23ef17647a12e437f8d22d5",
+    "url": "img/index-bg.1f66711a.jpg"
+  },
+  {
+    "revision": "490e73cd6f8fcef30256374d918d0afd",
+    "url": "img/logo.490e73cd.png"
+  },
+  {
+    "revision": "a72ceb9cd0bf4b88a3823bae3a96e4f9",
+    "url": "index.html"
+  },
+  {
+    "revision": "cec794fdabbfa4fcc424",
+    "url": "js/about.17b3719a.js"
+  },
+  {
+    "revision": "4f4a31017b00b318f671",
+    "url": "js/app.edae7b27.js"
+  },
+  {
+    "revision": "7495d540a965cd3f5528",
+    "url": "js/chunk-2d222708.8cf08c43.js"
+  },
+  {
+    "revision": "6b3fb8089d551f240ba4",
+    "url": "js/chunk-7d6732d4.e7da9c91.js"
+  },
+  {
+    "revision": "3b73c5e355af8fb39e95",
+    "url": "js/chunk-7f015059.e8944d5c.js"
+  },
+  {
+    "revision": "68f3c09036e274b8ce3b",
+    "url": "js/chunk-cde5e6e4.845faed9.js"
+  },
+  {
+    "revision": "aa31af4c5b75b9cb5d19",
+    "url": "js/chunk-de41ad9e.cfdec1b1.js"
+  },
+  {
+    "revision": "d2202a193fd8476d4f02",
+    "url": "js/chunk-vendors.2ac656ca.js"
+  },
+  {
+    "revision": "22291f2f0ff0861931a7a17915e88c36",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
